9e711e94e860b6c8ab5152d2226390a69614d079
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-07-11 10:30:00 +0200

    add colors and fonts

--- app/src/main/java/com/example/donut/ui/theme/Color.kt
+++ app/src/main/java/com/example/donut/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Pink90 = Color(0xFFFF7074)
+val Pink60 = Color(0xFFFF9494)
+val Pink30 = Color(0xFFFED8DF)
+val PrimaryText = Color(0x99000000)
+val BlueCard = Color(0xFFD7E4F6)
