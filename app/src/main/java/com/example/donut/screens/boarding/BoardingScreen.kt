6f3c4072b9a678f256b6685e4921508d7e5495c0
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-07-13 13:14:15 +0200

    boarding screen animation

--- app/src/main/java/com/example/donut/screens/boarding/BoardingScreen.kt
+++ app/src/main/java/com/example/donut/screens/boarding/BoardingScreen.kt
@@ -1,15 +1,22 @@
 package com.example.donut.screens.boarding
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -31,6 +38,7 @@ fun BoardingScreen(navController: NavHostController) {
         navController.navigateToHomeScreen()
     }
 }
+
 @Composable
 fun BoardingContent(
     navToHomeScreen: () -> Unit
@@ -40,10 +48,30 @@ fun BoardingContent(
             .fillMaxSize()
             .background(Pink30)
     ) {
+        val transition = rememberInfiniteTransition()
+        val offsetY by transition.animateFloat(
+            initialValue = -10f,
+            targetValue = 10f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(5000),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        val offsetX by transition.animateFloat(
+            initialValue = -20f,
+            targetValue = 10f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(5000),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
         Image(
             modifier = Modifier
                 .size(400.dp)
                 .padding(top = 64.dp)
+                .offset(x = offsetX.dp, y = offsetY.dp)
                 .align(Alignment.TopCenter)
                 .scale(1.8f),
             painter = painterResource(id = R.drawable.donuts),
